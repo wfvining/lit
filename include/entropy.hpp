18285a97d3689c2d67d470ff239764b0f5e43361
Will Vining <wfvining@gmail.com>
2019-02-19 18:59:16 -0700

    Joint entropy

--- include/entropy.hpp
+++ include/entropy.hpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <numeric>
 #include <cmath>
+#include <stdexcept>
 
 /**
  * Entropy in a single signal.
@@ -29,7 +30,27 @@ double entropy(const std::vector<T>& signal)
 template <typename T1, typename T2>
 double joint_entropy(const std::vector<T1>& signal1, const std::vector<T2>& signal2)
 {
-   return 0;
+   if(signal1.size() != signal2.size())
+   {
+      throw std::invalid_argument("samples must be the same length");
+   }
+   int n = signal1.size();
+
+   // build joint histogram
+   std::map<std::pair<T1,T2>, double> joint_hist;
+
+   auto s1 = signal1.begin();
+   auto s2 = signal2.begin();
+   while(s1 < signal1.end())
+   {
+      ++joint_hist[std::make_pair(*s1++, *s2++)];
+   }
+
+   return -std::accumulate(joint_hist.begin(), joint_hist.end(), 0.0,
+                           [n](double acc,
+                               std::pair<const std::pair<T1,T2>, double>& joint_count) {
+                              return acc + ((joint_count.second/n) * log2(joint_count.second/n));
+                           });
 }
 
 /**
--- test/entropy_test.cpp
+++ test/entropy_test.cpp
@@ -55,3 +55,46 @@ TEST(EntropyTest, enumRandomVariable)
    std::vector<RV> rv = {A, B, C, A, B, C, D, D};
    ASSERT_EQ(entropy(rv), 2.0);
 }
+
+TEST(EntropyTest, jointEntropyIndependent)
+{
+   std::vector<int> rv1;
+   std::vector<int> rv2;
+   rv1.push_back(0); rv2.push_back(1);
+   rv1.push_back(0); rv2.push_back(2);
+   rv1.push_back(1); rv2.push_back(1);
+   rv1.push_back(1); rv2.push_back(2);
+
+   ASSERT_EQ(joint_entropy(rv1, rv2), entropy(rv1)+entropy(rv2))
+      << "H(X,Y) = H(X) + H(Y) if X and Y are independent";
+}
+
+TEST(EntropyTest, jointEntropyDependent)
+{
+   std::vector<int> rv1;
+   std::vector<int> rv2;
+   // if rv1 = 0 -> rv2 = 1
+   rv1.push_back(0); rv2.push_back(1);
+   rv1.push_back(0); rv2.push_back(1);
+   rv1.push_back(1); rv2.push_back(2);
+   rv1.push_back(1); rv2.push_back(1);
+
+   ASSERT_LT(joint_entropy(rv1, rv2), entropy(rv1)+entropy(rv2))
+      << "joint entropy of dependent r.v.s should be less than the sum of their entropies.";
+}
+
+TEST(EntropyTest, jointEntropyWithSelf)
+{
+   std::vector<int> rv = {1,2,3,4,3,2,1};
+
+   ASSERT_EQ(joint_entropy(rv, rv), entropy(rv))
+      << "joint entropy of dependent r.v.s should be less than the sum of their entropies.";
+}
+
+TEST(EntropyTest, jointEntropyMismatchedSize)
+{
+   std::vector<int> rv1 = {1,34,1,56,1,5,6,3,5};
+   std::vector<int> rv2 = {1,4,12,54,2,1,1,1};
+   ASSERT_THROW(joint_entropy(rv1, rv2), std::invalid_argument);
+   ASSERT_THROW(joint_entropy(rv2, rv1), std::invalid_argument);
+}
